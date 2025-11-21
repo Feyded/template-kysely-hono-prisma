6d20fbc0004b808789cbfc8a58cb85445300ff8e
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2025-11-21 18:57:49 +0800

    feat: authentication using access token and refresh token

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -1,21 +1,46 @@
 import { UnauthorizedError } from "@/utils/error.js";
-import { verifyToken } from "@/utils/jwt.js";
+import {
+  generateAccessToken,
+  verifyAccessToken,
+  verifyRefreshToken,
+} from "@/utils/jwt.js";
 import type { Context, Next } from "hono";
+import { setCookie, getCookie } from "hono/cookie";
 
 export async function authenticationMiddleware(c: Context, next: Next) {
-  const accessToken = c.req.header("Authorization")?.split("Bearer ")[1];
+  let accessToken = getCookie(c, "auth__access_token");
 
-  if (!accessToken) {
-    throw new UnauthorizedError("token is required");
+  let verifiedAccessToken = accessToken ? await verifyAccessToken(accessToken) : null;
+
+  if (!verifiedAccessToken) {
+    const refreshToken = getCookie(c, "auth__refresh_token");
+
+    if (!refreshToken) {
+      throw new UnauthorizedError("refresh token is required");
+    }
+
+    const verifiedRefreshToken = await verifyRefreshToken(refreshToken);
+
+    if (!verifiedRefreshToken) {
+      throw new UnauthorizedError("Invalid Refresh token");
+    }
+
+    accessToken = await generateAccessToken({
+      id: verifiedRefreshToken.id as string,
+      email: verifiedRefreshToken.email as string,
+    });
+
+    setCookie(c, "auth__access_token", accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      path: "/",
+    });
+
+    verifiedAccessToken = await verifyAccessToken(accessToken);
   }
 
-  const verify = await verifyToken(accessToken);
+  c.set("user", verifiedAccessToken);
 
-  if (!verify) {
-    throw new UnauthorizedError("Invalid token");
-  }
-
-  c.set("user", verify);
-
-  await next();
+  return next();
 }
