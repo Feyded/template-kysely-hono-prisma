17d9f2e605da5b98bbe4853e02e14650460e8fcc
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2025-11-21 18:59:01 +0800

    refactor: remove uneccesary endpoint

--- src/controllers/auth/routes.ts
+++ src/controllers/auth/routes.ts
@@ -1,9 +1,8 @@
 import { Hono } from "hono";
-import { loginController, refreshTokenController } from "./index.js";
+import { loginController } from "./index.js";
 import { authenticationMiddleware } from "@/middlewares/authentication.js";
 
 const router = new Hono()
   .post("/auth/login", loginController)
-  .post("/refresh-token", refreshTokenController);
 
 export default router;
