36647fc0228cee04207132f51130fa6aef3176f1
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2025-11-21 18:58:42 +0800

    feat: use cors

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,16 @@ import { serveStatic } from "@hono/node-server/serve-static";
 import { routes } from "./controllers/routes.js";
 import { errorHandlerMiddleware } from "./middlewares/error-handler.js";
 import { envConfig } from "./env.js";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend origin
+    credentials: true, // allow cookies
+  })
+);
 
 app.onError(errorHandlerMiddleware);
 app.use("/uploads/*", serveStatic({ root: "./public" }));
